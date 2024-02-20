182c3de09a5a8d9ce76eb16ca27141066bb969bb
raheemudheen M A <raheemudheenma118@gmail.com>
2024-02-21 01:28:13 +0530

    Update Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,11 +9,11 @@ interface prpsType{
 
 const Card = ({movieID,MovieTitle,MoviePosterIMG,releaseDate}:prpsType) => {
 
-    const[loading,setLoading]=useState(false)
-const [error, seterror] = useState(false)
+ const[loading,setLoading]=useState(false)
+ const [error, seterror] = useState(false)
   return (
     <div>
-        
+        Card
     </div>
   )
 }
